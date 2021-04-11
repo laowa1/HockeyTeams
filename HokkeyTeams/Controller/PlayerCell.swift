b3be8937d52a7b47db65093578a47248f68de921
Laowai <k.zhalgas@icloud.com>
2021-04-12 03:52:18 +0600

    configuration PlayerViewController configuration URLSession

--- HokkeyTeams/Controller/PlayerCell.swift
+++ HokkeyTeams/Controller/PlayerCell.swift
@@ -13,6 +13,18 @@ class PlayerCell: UITableViewCell {
     @IBOutlet weak var playerName: UILabel!
     @IBOutlet weak var playerNumber: UILabel!
     
-   
-
+    func configure(with player: Player) {
+        playerTeam.text = player.team.name ?? "Команда не найдено"
+        playerName.text = player.name ?? "Имя не найдено"
+        playerNumber.text = "N\(player.shirt_number ?? 0)"
+        
+        DispatchQueue.global().async {
+            guard let image = player.image else { return}
+            guard let imageUrl = URL(string: image) else { return }
+            guard let imageData = try? Data(contentsOf: imageUrl) else { return }
+            DispatchQueue.main.async {
+                self.playerImage.image = UIImage(data: imageData)
+            }
+        }
+    }
 }
--- HokkeyTeams/Model/Player.swift
+++ HokkeyTeams/Model/Player.swift
@@ -8,5 +8,12 @@
 import Foundation
 
 struct Player: Decodable {
-    
+        let shirt_number: Int?
+        let name: String?
+        let image: String?
+        let team: PlayerTeam
+    }
+
+    struct PlayerTeam: Codable {
+        let name: String?
 }
